c13dbe8fa34c115a34c9fbc925aaeb583428350a
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-25 16:08:57 +0300

    Вроде готово

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры М чисел.
+// Подсчитайте, сколько чисел больше 0 ввел пользователь
+
+int[] InputArray() {
+    int [] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+    return array;
+} 
+
+void ReleaseArray(int[] a) {
+    int count = 0;
+    foreach (int x in a) {
+        if (x > 0) count++;
+    }
+    Console.WriteLine($"->{count}"); 
+}
+
+Console.Clear(); 
+int[] array = InputArray();
+ReleaseArray(array);
---
+++ Task43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43. Напишите программу, которая найдет точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2
+// Значения k1, b1, k2 и b2 задаются пользователем
+
+void Find(double kk1, double bb1, double kk2, double bb2) {
+    double x = (bb2 - bb1)/(kk1 - kk2);
+    double y = kk1 * x + bb1; 
+    Console.WriteLine($"({Math.Round(x,2)};{Math.Round(y,2)})"); 
+}
+
+Console.Clear(); 
+double k1, k2, b1, b2;
+Console.Write("Введите k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+Find(k1, b1, k2, b2);
